03163af5c863006c5b0607be1e7e2d824dd38411
Russell Cloran <rcloran@gmail.com>
2023-10-16 11:51:07 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# AXP192 Power Management IC, Rust library
+
+## Example
+
+`examples/m5stack-core2.rs` demonstrates configuring and reading from the
+AXP192 on an [M5Stack Core2](https://docs.m5stack.com/en/core/core2).
+
+This example uses the [Rust on ESP](https://esp-rs.github.io/book/) toolchain
+and [esp32-hal crate](https://crates.io/crates/esp32-hal), which requires [some
+setup](https://esp-rs.github.io/book/installation/).
+
+You can run this example on your device with `cargo run --example m5stack-core2`.
---
+++ examples/m5stack-core2.rs
@@ -0,0 +1,98 @@
+#![no_std]
+#![no_main]
+
+use axp192;
+use embedded_hal as eh;
+use esp32_hal::{
+    clock::ClockControl, gpio::IO, i2c::I2C, peripherals::Peripherals, prelude::*, Delay,
+};
+use esp_backtrace as _;
+use esp_println::println;
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take();
+    let mut system = peripherals.DPORT.split();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+    let mut delay = Delay::new(&clocks);
+
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+
+    let i2c = I2C::new(
+        peripherals.I2C0,
+        io.pins.gpio21,
+        io.pins.gpio22,
+        400u32.kHz(),
+        &mut system.peripheral_clock_control,
+        &clocks,
+    );
+
+    let mut axp = axp192::Axp192::new(i2c);
+    m5sc2_init(&mut axp, &mut delay).unwrap();
+
+    loop {
+        let bat_v = axp.get_battery_voltage().unwrap();
+        let bat_ch_a = axp.get_battery_charge_current().unwrap();
+        let bat_disch_a = axp.get_battery_discharge_current().unwrap();
+        let usb_v = axp.get_acin_voltage().unwrap();
+        let usb_a = axp.get_acin_current().unwrap();
+        let temp = axp.get_internal_temperature().unwrap();
+        println!("Bat {bat_v}v ch {bat_ch_a}A disch {bat_disch_a}A");
+        println!("USB {usb_v}v @ {usb_a}A");
+        println!("Internal temperature: {temp}°C");
+
+        delay.delay_ms(1000u32);
+    }
+}
+
+fn m5sc2_init<I2C, E>(axp: &mut axp192::Axp192<I2C>, delay: &mut Delay) -> Result<(), E>
+where
+    I2C: eh::blocking::i2c::Read<Error = E>
+        + eh::blocking::i2c::Write<Error = E>
+        + eh::blocking::i2c::WriteRead<Error = E>,
+{
+    // Default setup for M5Stack Core 2
+    axp.set_dcdc1_voltage(3350)?; // Voltage to provide to the microcontroller (this one!)
+
+    axp.set_ldo2_voltage(3300)?; // Peripherals (LCD, ...)
+    axp.set_ldo2_on(true)?;
+
+    axp.set_ldo3_voltage(2000)?; // Vibration motor
+    axp.set_ldo3_on(false)?;
+
+    axp.set_dcdc3_voltage(2800)?; // LCD backlight
+    axp.set_dcdc3_on(true)?;
+
+    axp.set_gpio1_mode(axp192::GpioMode12::NmosOpenDrainOutput)?; // Power LED
+    axp.set_gpio1_output(false)?; // In open drain modes, state is opposite to what you might
+                                  // expect
+
+    axp.set_gpio2_mode(axp192::GpioMode12::NmosOpenDrainOutput)?; // Speaker
+    axp.set_gpio2_output(true)?;
+
+    axp.set_key_mode(
+        // Configure how the power button press will work
+        axp192::ShutdownDuration::Sd4s,
+        axp192::PowerOkDelay::Delay64ms,
+        true,
+        axp192::LongPress::Lp1000ms,
+        axp192::BootTime::Boot512ms,
+    )?;
+
+    axp.set_gpio4_mode(axp192::GpioMode34::NmosOpenDrainOutput)?; // LCD reset control
+
+    axp.set_battery_voltage_adc_enable(true)?;
+    axp.set_battery_current_adc_enable(true)?;
+    axp.set_acin_current_adc_enable(true)?;
+    axp.set_acin_voltage_adc_enable(true)?;
+
+    // Actually reset the LCD
+    axp.set_gpio4_output(false)?;
+    axp.set_ldo3_on(true)?; // Buzz the vibration motor while intializing ¯\_(ツ)_/¯
+    delay.delay_ms(100u32);
+    axp.set_gpio4_output(true)?;
+    axp.set_ldo3_on(false)?;
+    delay.delay_ms(100u32);
+
+    Ok(())
+}
